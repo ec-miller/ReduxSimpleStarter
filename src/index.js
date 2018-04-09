a80770216431a7586bd2a4ce3ad0c8b824614ae7
Eric <ericmiller@umn.edu>
2018-04-08 20:51:15 -0500

    Video player almost finished

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar'
+import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 
 const API_KEY = 'AIzaSyBCLuhdcvlUFhyks1mFeZ5ejaratqqMpw4'
 
 //Create a new component. This should create some HTML.
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('fkj & masego');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-//put this in the DOM
-ReactDOM.render(<App />, document.querySelector('.container')); //Instantiate
+//instantiate and add to the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
