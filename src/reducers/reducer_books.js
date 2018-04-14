419d85700294d900338eeba0274772e3183264d0
Eric <ericmiller@umn.edu>
2018-04-13 21:42:04 -0500

    starting out

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+function() {
+  return [
+    { title: "You Don't Know JS" },
+    { title: 'Seven Habits of Highly Effective People' },
+    { title: 'The Davinci Code' },
+    { title: 'Rework'},
+    { title: 'Wicky-Wok'}
+  ]
+}
