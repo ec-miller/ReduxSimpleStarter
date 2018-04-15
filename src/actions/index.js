a8eff124e0348b8e9ccf09cbc28466da2dfedefe
Eric <ericmiller@umn.edu>
2018-04-15 15:56:37 -0500

    search bar added

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '7f6306190c357dd28eea6bfe7c0e831d';
--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../containers/search_bar'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div className="bump"></div>
+        <SearchBar />
+      </div>
     );
   }
 }
